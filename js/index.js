2108b062b28ad8ee16722e676043b50c62d76bb0
Emilie Rose <embi0003@stud.kea.dk>
2025-02-17 14:25:58 +0100

    add forside

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
-console.log("Script loaded");
+// console.log("Script loaded");
 
-fetch("https://dummyjson.com/recipes")
-  .then((res) => res.json())
-  .then(showRecipe);
+// fetch("https://dummyjson.com/recipes")
+//   .then((res) => res.json())
+//   .then(showRecipe);
 
-function showRecipe(data) {
-  console.log("mine data er:", data);
+// function showRecipe(data) {
+//   console.log("mine data er:", data);
 
-  const markup = data
-    .map(
-      (element) => `<a class="link" href="produktliste.html?recipe=${element.recipe}">
-          <img class="index_cirkel" src="https://cdn.dummyjson.com/recipe-images/${recipe.id}webp" alt="Dinner" />
-          Dinner ${element.recipe}
-        </a>`
-    )
+//   const markup = data.recipes
+//     .map(
+//       (element) => `<a href="produktliste.html?recipe=${element.id}" class="link">
+//           <img class="index_cirkel" src="https://cdn.dummyjson.com/recipe-images/${element.id}.webp" alt="Dinner" />
+//           Dinner ${element.id}
+//         </a>`
+//     )
 
-    .join("");
+//     .join("");
 
-  console.log("min markup er ", markup);
-  document.querySelector(".category_list_container").innerHTML = markup;
-}
+//   console.log("min markup er ", markup);
+//   document.querySelector(".index_container").innerHTML = markup;
+// }
